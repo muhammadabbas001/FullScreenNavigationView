58282962e95e61d400e0de519be2995b800d9527
Zee Shan <>
2022-10-01 17:24:16 -0500

    little tweak into the code

--- FullScreenNavigationView/Main/Home/SubViews/SecondView.swift
+++ FullScreenNavigationView/Main/Home/SubViews/SecondView.swift
@@ -9,23 +9,20 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        NavigationView{
-            VStack(spacing: 20){
-                Spacer()
-                Text("Second View!")
-                NavigationLink {
-                    ThirdView(isThirdView: true)
-                } label: {
-                    Text("Navigate to ThirdView")
-                }
-                Button {
-                    AppEventsManager.shared.navigateFullScreen.send((false, .empty))
-                } label: {
-                    Text("Back")
-                }
-                Spacer()
+        VStack(spacing: 20){
+            Spacer()
+            Text("Second View!")
+            NavigationLink {
+                ThirdView(isThirdView: true)
+            } label: {
+                Text("Navigate to ThirdView")
             }
-            .navigationBarHidden(true)
+            Button {
+                AppEventsManager.shared.navigateFullScreen.send((false, .empty))
+            } label: {
+                Text("Back")
+            }
+            Spacer()
         }
     }
 }
